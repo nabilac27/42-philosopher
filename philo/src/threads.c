29af8ddb1addea9650b3666baf8f4f3670e146f1
Nabila Chairunnisa <nabilac@Nabilas-MacBook-Air.local>
2025-08-02 13:56:45 +0200

    feat: meal_round_finished()

--- philo/src/threads.c
+++ philo/src/threads.c
@@ -6,7 +6,7 @@
 /*   By: nchairun <nchairun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 15:19:00 by nchairun          #+#    #+#             */
-/*   Updated: 2025/07/30 13:37:06 by nchairun         ###   ########.fr       */
+/*   Updated: 2025/08/02 13:55:44 by nchairun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	*thread_monitor(void *table);
 void	*thread_routine(void *table);
+bool	meal_round_finished(t_table *table);
+bool	philo_died(t_table *table);
 
 bool	setup_thread(t_table *table)
 {
@@ -56,9 +58,9 @@ void	*thread_monitor(void *table)
 {
    t_table *p_table = (t_table *)table;
 	
-	while (table != NULL)
+	while (p_table != NULL)
 	{
-		if (meal_round_finished() || philo_died())
+		if (meal_round_finished(p_table) || philo_died(p_table))
 			break ;
 	}
 	printf("thread_monitor\n");
@@ -74,7 +76,59 @@ void	*thread_routine(void *philo)
 	return NULL;
 }
 
-bool	meal_round_finished(t_data *table)
+bool	meal_round_finished(t_table *table)
 {
+	int	i;
+	int	philos_finished_eating;
 
+	if (table->num_must_meals == -1)
+		return (false);
+
+	pthread_mutex_lock(&table->meal_mutex);
+	philos_finished_eating = 0;
+	i = 0;
+	while (i < table->num_philos)
+	{
+		if (table->philos[i].num_eaten_meals >= table->num_must_meals)
+			philos_finished_eating++;
+		i++;
+	}
+	pthread_mutex_unlock(&table->meal_mutex);
+
+	return (philos_finished_eating == table->num_philos);
+}
+
+bool	philo_died(t_table *table)
+{
+	int			i;
+	long long	current_time;
+	struct timeval	time;
+
+	pthread_mutex_lock(&table->death_mutex);
+	if (table->death_philo != 0)
+	{
+		pthread_mutex_unlock(&table->death_mutex);
+		return (true);
+	}
+	pthread_mutex_unlock(&table->death_mutex);
+
+	gettimeofday(&time, NULL);
+	current_time = (time.tv_sec * 1000) + (time.tv_usec / 1000);
+
+	pthread_mutex_lock(&table->meal_mutex);
+	i = 0;
+	while (i < table->num_philos)
+	{
+		if (current_time - table->philos[i].last_meal_time > table->time_to_die)
+		{
+			pthread_mutex_unlock(&table->meal_mutex);
+			pthread_mutex_lock(&table->death_mutex);
+			table->death_philo = table->philos[i].id;
+			pthread_mutex_unlock(&table->death_mutex);
+			return (true);
+		}
+		i++;
+	}
+	pthread_mutex_unlock(&table->meal_mutex);
+	return (false);
 }
